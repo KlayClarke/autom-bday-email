77d6fe054ba59ac06ef744c2b802093206083225
Klay Clarke <klayaclarke@gmail.com>
2021-11-27 12:08:08 -0500

    create dictionary using dataframe of birthday csv - and if today's date (as a tuple) is present in dictionary (as the key), send email of randomly chosen letter to that person / email address present as values of said key
    replace [NAME] placeholder in letter with name of person

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ now = dt.datetime.now()
 current_month = now.month
 
 # find out which day of the month it is
-current_day_of_the_month = now.day
+current_day = now.day
 
 # find out which day of the week it is
 current_day_of_week = now.weekday()
+
+today = (current_month, current_day)
 
 # open quotes.txt file and transfer all into a list
 
@@ -22,16 +24,22 @@ my_password = 'klayclarkecodetest'
 
 # check to see if birthday in birthdays.csv matches today's date
 
+# create data frame from csv
+
 df = pd.read_csv('birthdays.csv')
 
-df.set_index('name', inplace=True)
+# create dictionary from data frame using dict comprehension
 
-klay_data = df.loc['Klay', :]
+birthdays_dict = {(row.month, row.day): row for (index, row) in df.iterrows()}
 
-if klay_data['day'] == current_day_of_the_month and klay_data['month'] == current_month:
+# if today's date is present in birthdays_dict (means that someone's birthday is today) - send person a birthday email
+
+if today in birthdays_dict:
+    birthday_name = birthdays_dict[today]['name']
+    birthday_email = birthdays_dict[today]['email']
     random_letter = random.choice(os.listdir('letter_templates'))
     with open(f'letter_templates/{random_letter}') as file:
-        letter = file.read().replace('[NAME]', 'Klay')
+        letter = file.read().replace('[NAME]', birthday_name)
         print(letter)
     with smtplib.SMTP('smtp.gmail.com') as connection:
         # make connection secure / encrypted
@@ -40,5 +48,5 @@ if klay_data['day'] == current_day_of_the_month and klay_data['month'] == curren
         connection.login(user=my_email, password=my_password)
         # to send mail and message
         connection.sendmail(from_addr=my_email,
-                            to_addrs='klayaclarke@gmail.com',
+                            to_addrs=birthday_email,
                             msg=f'Subject:Happy Birthday\n\n{letter}')
