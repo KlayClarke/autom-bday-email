590e9c48ede2d3a4a93fd97cf0c3150ace5feee2
Klay Clarke <klayaclarke@gmail.com>
2021-11-27 09:22:11 -0500

    change email msg to add subject - prevent email classification as spam

--- main.py
+++ main.py
@@ -10,5 +10,7 @@ connection.starttls()
 # to login to email service
 connection.login(user=my_email, password=my_password)
 # to send mail and message
-connection.sendmail(from_addr=my_email, to_addrs='klayaclarke@gmail.com', msg='Hello world!')
+connection.sendmail(from_addr=my_email,
+                    to_addrs='klayaclarke@gmail.com',
+                    msg='Subject:Code Test\n\nHello world!')
 connection.close()
