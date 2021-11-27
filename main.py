1d956c1e200a2d8c30c491403e63e93590e764a5
Klay Clarke <klayaclarke@gmail.com>
2021-11-27 09:49:50 -0500

    tap into datetime package to get current day of week

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import smtplib
+import datetime as dt
 
-my_email = 'kccodetest@gmail.com'
-my_password = 'klayclarkecodetest'
-
-with smtplib.SMTP('smtp.gmail.com') as connection:
-    # make connection secure / encrypted
-    connection.starttls()
-    # to login to email service
-    connection.login(user=my_email, password=my_password)
-    # to send mail and message
-    connection.sendmail(from_addr=my_email,
-                        to_addrs='klayaclarke@gmail.com',
-                        msg='Subject:Code Test\n\nHello world!')
+# my_email = 'kccodetest@gmail.com'
+# my_password = 'klayclarkecodetest'
+#
+# with smtplib.SMTP('smtp.gmail.com') as connection:
+#     # make connection secure / encrypted
+#     connection.starttls()
+#     # to login to email service
+#     connection.login(user=my_email, password=my_password)
+#     # to send mail and message
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs='klayaclarke@gmail.com',
+#                         msg='Subject:Code Test\n\nHello world!')
+now = dt.datetime.now()
+current_day_of_week = now.weekday()
+print(current_day_of_week)
