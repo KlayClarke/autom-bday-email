3aaea7bc20234f4198cd63e1c85c1a3521b23e90
Klay Clarke <klayaclarke@gmail.com>
2021-11-27 10:45:19 -0500

    if birthday of Klay (an entry of birthdays.csv) is equal to the current day and month, randomly choose a letter from our 'letter_templates' folder and replace the '[NAME]' placeholder in the letter with 'Klay' - then send said letter to Klay's email address

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
 import smtplib
 import random
 import datetime as dt
 import pandas as pd
 
-# find out which day of the week it is
-
 now = dt.datetime.now()
+
+# find out which month it is
+current_month = now.month
+
+# find out which day of the month it is
+current_day_of_the_month = now.day
+
+# find out which day of the week it is
 current_day_of_week = now.weekday()
 
 # open quotes.txt file and transfer all into a list
@@ -13,30 +20,25 @@ current_day_of_week = now.weekday()
 my_email = 'kccodetest@gmail.com'
 my_password = 'klayclarkecodetest'
 
-# if day of week is Monday, send self a motivational quote
-
-# if current_day_of_week == 0:
-#     with open('quotes.txt', mode='r') as file:
-#         list_of_quotes = file.readlines()
-#
-#     random_quote = random.choice(list_of_quotes)
-#
-#     with smtplib.SMTP('smtp.gmail.com') as connection:
-#         # make connection secure / encrypted
-#         connection.starttls()
-#         # to login to email service
-#         connection.login(user=my_email, password=my_password)
-#         # to send mail and message
-#         connection.sendmail(from_addr=my_email,
-#                             to_addrs='klayaclarke@gmail.com',
-#                             msg=f'Subject:Quote Of The Day\n\n{random_quote}')
-
 # check to see if birthday in birthdays.csv matches today's date
 
 df = pd.read_csv('birthdays.csv')
 
-print(df)
-
 df.set_index('name', inplace=True)
 
-print(df)
+klay_data = df.loc['Klay', :]
+
+if klay_data['day'] == current_day_of_the_month and klay_data['month'] == current_month:
+    random_letter = random.choice(os.listdir('letter_templates'))
+    with open(f'letter_templates/{random_letter}') as file:
+        letter = file.read().replace('[NAME]', 'Klay')
+        print(letter)
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        # make connection secure / encrypted
+        connection.starttls()
+        # to login to email service
+        connection.login(user=my_email, password=my_password)
+        # to send mail and message
+        connection.sendmail(from_addr=my_email,
+                            to_addrs='klayaclarke@gmail.com',
+                            msg=f'Subject:Happy Birthday\n\n{letter}')
