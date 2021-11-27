ae294aca16e1ba56d0af4d4454445dd046a9ff70
Klay Clarke <klayaclarke@gmail.com>
2021-11-27 09:20:26 -0500

    send email using Python smtplib package

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import smtplib
+
+my_email = 'kccodetest@gmail.com'
+my_password = 'klayclarkecodetest'
+
+connection = smtplib.SMTP('smtp.gmail.com')
+
+# make connection secure / encrypted
+connection.starttls()
+# to login to email service
+connection.login(user=my_email, password=my_password)
+# to send mail and message
+connection.sendmail(from_addr=my_email, to_addrs='klayaclarke@gmail.com', msg='Hello world!')
+connection.close()
