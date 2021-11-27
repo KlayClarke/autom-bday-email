07fe2e4cd1076bf48e252483a122f5d34c469666
Klay Clarke <klayaclarke@gmail.com>
2021-11-27 10:03:08 -0500

    change code so email will send only if current day of the week is Monday

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ import datetime as dt
 
 now = dt.datetime.now()
 current_day_of_week = now.weekday()
-print(current_day_of_week)
 
 # open quotes.txt file and transfer all into a list
-
-with open('quotes.txt', mode='r') as file:
-    list_of_quotes = file.readlines()
-
-random_quote = random.choice(list_of_quotes)
 
 my_email = 'kccodetest@gmail.com'
 my_password = 'klayclarkecodetest'
 
-with smtplib.SMTP('smtp.gmail.com') as connection:
-    # make connection secure / encrypted
-    connection.starttls()
-    # to login to email service
-    connection.login(user=my_email, password=my_password)
-    # to send mail and message
-    connection.sendmail(from_addr=my_email,
-                        to_addrs='klayaclarke@gmail.com',
-                        msg=f'Subject:Quote Of The Day\n\n{random_quote}')
+if current_day_of_week == 0:
+    with open('quotes.txt', mode='r') as file:
+        list_of_quotes = file.readlines()
+
+    random_quote = random.choice(list_of_quotes)
+
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        # make connection secure / encrypted
+        connection.starttls()
+        # to login to email service
+        connection.login(user=my_email, password=my_password)
+        # to send mail and message
+        connection.sendmail(from_addr=my_email,
+                            to_addrs='klayaclarke@gmail.com',
+                            msg=f'Subject:Quote Of The Day\n\n{random_quote}')
