8c050447daed185c162706208af79d8281980e51
Klay Clarke <klayaclarke@gmail.com>
2021-11-27 09:24:28 -0500

    use 'with' method to automatically close connection

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import smtplib
 my_email = 'kccodetest@gmail.com'
 my_password = 'klayclarkecodetest'
 
-connection = smtplib.SMTP('smtp.gmail.com')
-
-# make connection secure / encrypted
-connection.starttls()
-# to login to email service
-connection.login(user=my_email, password=my_password)
-# to send mail and message
-connection.sendmail(from_addr=my_email,
-                    to_addrs='klayaclarke@gmail.com',
-                    msg='Subject:Code Test\n\nHello world!')
-connection.close()
+with smtplib.SMTP('smtp.gmail.com') as connection:
+    # make connection secure / encrypted
+    connection.starttls()
+    # to login to email service
+    connection.login(user=my_email, password=my_password)
+    # to send mail and message
+    connection.sendmail(from_addr=my_email,
+                        to_addrs='klayaclarke@gmail.com',
+                        msg='Subject:Code Test\n\nHello world!')
