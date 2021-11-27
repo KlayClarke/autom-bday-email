00a73b1d32b8e18a35997e1ae894be2e2bcff1c0
Klay Clarke <klayaclarke@gmail.com>
2021-11-27 10:30:15 -0500

    read csv using pandas and change dataframe index to the names of each entry in our csv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import smtplib
 import random
 import datetime as dt
+import pandas as pd
 
 # find out which day of the week it is
 
@@ -12,18 +13,30 @@ current_day_of_week = now.weekday()
 my_email = 'kccodetest@gmail.com'
 my_password = 'klayclarkecodetest'
 
-if current_day_of_week == 0:
-    with open('quotes.txt', mode='r') as file:
-        list_of_quotes = file.readlines()
+# if day of week is Monday, send self a motivational quote
 
-    random_quote = random.choice(list_of_quotes)
+# if current_day_of_week == 0:
+#     with open('quotes.txt', mode='r') as file:
+#         list_of_quotes = file.readlines()
+#
+#     random_quote = random.choice(list_of_quotes)
+#
+#     with smtplib.SMTP('smtp.gmail.com') as connection:
+#         # make connection secure / encrypted
+#         connection.starttls()
+#         # to login to email service
+#         connection.login(user=my_email, password=my_password)
+#         # to send mail and message
+#         connection.sendmail(from_addr=my_email,
+#                             to_addrs='klayaclarke@gmail.com',
+#                             msg=f'Subject:Quote Of The Day\n\n{random_quote}')
 
-    with smtplib.SMTP('smtp.gmail.com') as connection:
-        # make connection secure / encrypted
-        connection.starttls()
-        # to login to email service
-        connection.login(user=my_email, password=my_password)
-        # to send mail and message
-        connection.sendmail(from_addr=my_email,
-                            to_addrs='klayaclarke@gmail.com',
-                            msg=f'Subject:Quote Of The Day\n\n{random_quote}')
+# check to see if birthday in birthdays.csv matches today's date
+
+df = pd.read_csv('birthdays.csv')
+
+print(df)
+
+df.set_index('name', inplace=True)
+
+print(df)
